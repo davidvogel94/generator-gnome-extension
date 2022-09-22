ea5073639babde149892465c98df61432e77f906
David Vogel <dvogel616@gmail.com>
2022-09-22 19:42:56 +1000

    feat: added reference to gnome-shell-extension-types package to allow IDE autocompletion etc.

--- generators/app/templates/src/extension.ts
+++ generators/app/templates/src/extension.ts
@@ -1,3 +1,28 @@
-function init () {}
-function enable () {}
-function disable() {}
+/// <reference path="../node_modules/gnome-shell-extension-types/global.d.ts"/>
+
+const {St, Clutter} = imports.gi;
+const Main = imports.ui.main;
+
+let panelButton: StButton;
+
+function init () {
+    // Create a Button with "Hello World" text
+    let panelButtonText = new St.Label({
+        text : "Hello World",
+        y_align: Clutter.ActorAlign.CENTER,
+    });
+    panelButton = new St.Button({
+        style_class : "panel-button",
+        child: panelButtonText
+    });
+}
+
+function enable () {
+    // Add the button to the panel
+    Main.panel._rightBox.insert_child_at_index(panelButton, 0);
+}
+
+function disable () {
+    // Remove the added button from panel
+    Main.panel._rightBox.remove_child(panelButton);
+}
